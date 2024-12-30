568ea6bfacfa6d1d5379bbc276233091a2201999
7532602 <7532602@gmail.com>
2024-12-30 22:00:30 +0900

    MemberService 로직 구현

--- jpashop_v2/src/main/java/jpabook/jpashop/Repository/MemberRepository.java
+++ jpashop_v2/src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -3,15 +3,18 @@ package jpabook.jpashop.Repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    // 스프링 부트에서만 @PersistenceContext 대신 @Autowired를 사용할 수 있도록 지원해준다.
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
