3e7bafdebdb0789b0e50d29cd84375c0a0c4f8a7
7532602 <7532602@gmail.com>
2025-01-21 22:22:49 +0900

    update 변경감지

--- jpashop_v2/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop_v2/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.Repository.ItemRepository;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        // 영속성 컨텍스트에서 관리되기 때문에 변화된 사항들에 대해 save할 필요가 없다.
     }
 
     public List<Item> findItems() {
